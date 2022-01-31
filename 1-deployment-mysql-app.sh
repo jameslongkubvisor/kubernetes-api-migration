510f848a117ef441ad87be6a22bb3567ec507e93
jameslongkubvisor <james@Jamess-MacBook-Pro.local>
2022-02-01 01:21:01 +0200

    Deployment changes

--- 1-deployment-mysql-app.sh
+++ 1-deployment-mysql-app.sh
@@ -8,3 +8,9 @@ kubectl apply -f ./docker/app/app-deployment-mysql.yml
 
 ## Port Forward to Expose MySql Database
 # kubectl port-forward service/mysql 3306:3306
+
+## Follow the logs of the Java Application [ mysql ]
+# kubectl logs -f deployment/user-rest-api-mysql
+
+## Port Forward to Expose Running Java Application [ mysql ]
+# kubectl port-forward service/user-rest-api-mysql 9000:9000
--- 2-deployment-postgres-agent-app.sh
+++ 2-deployment-postgres-agent-app.sh
@@ -10,9 +10,10 @@ kubectl apply -f docker/postgres/postgres-deployment.yml
 kubectl apply -f docker/agent/agent-deployment.yml
 
 ## Run Migration Agent Command
-kubectl exec migration-agent -- pgloader mysql://root:password@mysql/test postgresql://spring-user:root@localhost/
+kubectl exec -i -t deployment/migration-agent -- pgloader mysql://root:password@mysql/test postgresql://spring-user:root@localhost/
 
 ## Deploy Application Server [Postgres]
 kubectl apply -f docker/app/app-deployment-postgres.yml
 
-
+## Follow the logs of the Java Application [ mysql ]
+kubectl logs -f deployment/user-rest-api-postgres
