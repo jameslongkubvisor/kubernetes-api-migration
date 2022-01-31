7487eef172427072d1d9a932d177cf1d71318c50
jameslongkubvisor <james@Jamess-MacBook-Pro.local>
2022-02-01 00:29:16 +0200

    Port Binding Control for DB

--- 1-deployment-mysql-app.sh
+++ 1-deployment-mysql-app.sh
@@ -6,3 +6,5 @@ kubectl apply -f ./docker/mysql/mysql-deployment.yml
 ## Deploy Application Server [MySql]
 kubectl apply -f ./docker/app/app-deployment-mysql.yml
 
+## Port Forward to Expose MySql Database
+# kubectl port-forward service/mysql 3306:3306
--- 2-deployment-postgres-agent-app.sh
+++ 2-deployment-postgres-agent-app.sh
@@ -3,6 +3,9 @@
 ## Deploy Postgres Server
 kubectl apply -f docker/postgres/postgres-deployment.yml
 
+## Port Forward to Expose Postgres Database
+# kubectl port-forward service/postgres 5432:5432
+
 ## Deploy Migration Agent 
 kubectl apply -f docker/agent/agent-deployment.yml
 
