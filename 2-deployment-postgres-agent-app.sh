1791affa37d141a46e28d5bbe1de30e3c6d01fbb
jameslongkubvisor <james@Jamess-MacBook-Pro.local>
2022-01-31 23:16:58 +0200

    Deployment Process Postgres -> App

---
+++ 2-deployment-postgres-agent-app.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+## Deploy Postgres Server
+kubectl apply -f docker/postgres/postgres-deployment.yml
+
+## Deploy Migration Agent 
+kubectl apply -f docker/agent/agent-deployment.yml
+
+## Run Migration Agent Command
+kubectl exec migration-agent -- pgloader mysql://root:password@mysql/test postgresql://spring-user:root@localhost/
+
+## Deploy Application Server [Postgres]
+kubectl apply -f docker/app/app-deployment-postgres.yml
+
+
