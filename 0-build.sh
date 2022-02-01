a254d61f5e0f03cb5f65186b1ae7131d33ae5167
jameslongkubvisor <james@Jamess-MacBook-Pro.local>
2022-02-01 19:23:50 +0200

    Updated build with Postgrest

--- 0-build.sh
+++ 0-build.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-## Clone Java Rest API Applicaiton
+## Clone Java Rest API [ Mysql ] Applicaiton
 git clone https://github.com/wagnerjfr/sprinboot-rest-mysql.git
 
 ## Enter Directory
@@ -20,6 +20,23 @@ bash ./mvnw clean package docker:build -DskipTests
 
 ## Exit Directory [ Back to File Root ]
 cd ..
+
+## Clone Java Rest API [ Postgres ] Applicaiton
+## Enter Directory
+cd sprinboot-rest-postgres
+
+## Install Dependencies [ Skip Tests as it causes Build to fail]
+bash ./mvnw install -DskipTests
+
+## Build
+bash ./mvnw clean package -DskipTests
+
+## Initial Start [ MySql -> Load User]
+# bash ./mvnw clean spring-boot:run
+
+## Build the Java Rest API Application + Docker Image [ not used ]
+bash ./mvnw clean package docker:build -DskipTests
+
 
 ## Build All Docker Container and Tag them
 docker-compose build 
