4b1c8f999340b1d1b14352f5517415dec56b4990
jameslongkubvisor <james@Jamess-MacBook-Pro.local>
2022-02-01 20:18:43 +0200

    Fixed User Query Issue

--- 3-migrate-data.sh
+++ 3-migrate-data.sh
@@ -8,3 +8,7 @@ kubectl port-forward service/postgres 5432:5432
 
 ## Migrate Data from Mysql to Postgres
 pgloader mysql://root:password@localhost:3306/test postgresql://spring-user:secret@localhost:5432/test
+
+## Port Forward to Expose Running Java Application [ mysql ]
+# kubectl port-forward service/user-rest-api-mysql 9000:9000
+# kubectl port-forward service/user-rest-api-postgres 9001:9001
--- sprinboot-rest-postgres/src/main/java/com/mycompany/springrestmysql/model/User.java
+++ sprinboot-rest-postgres/src/main/java/com/mycompany/springrestmysql/model/User.java
@@ -5,13 +5,15 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Data
 @NoArgsConstructor
-@Entity(name="\"user\"")
+@Entity
+@Table(name="\"user\"")
 public class User {
 
     @Id
